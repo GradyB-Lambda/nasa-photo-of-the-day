b5d9ab3779a085fe4637cc5d372fb1785f85442e
unknown <gradybollas@gmail.com>
2020-12-10 07:28:16 -0800

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
+import Images from './Images'
 import "./App.css";
 
+const nasaApi=  "https://api.nasa.gov/planetary/apod?api_key=vlOLRUDS806lmY6WhkoMwdV94fXNc9FgcuBkffoD.nasa.gov/planetary/apod?api_key=6s8lcv8nth7JkF1rcx3Ng8jGwvWcXbFdGb4VNbcy";
+
 function App() {
+  const [apod, setApod] =useState([]);
+
+  useEffect(()=>{
+    const fetchImage = () => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=vlOLRUDS806lmY6WhkoMwdV94fXNc9FgcuBkffoD`)
+      .then(res => {
+        apod(res.data)
+      })
+      .catch(err => {
+        
+      })
+    }
+    fetchImage()
+},[])
   return (
     <div className="App">
+      <h1>Today's Photo</h1>
+     
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+       Our world is bigger than we're used to. There is endless space. Explore!  <span role="img" aria-label='go!'>🚀</span>!</p>
+      
+        <img id="mainPic" src = {apod.url}></img>
+        
+     <p class="mainText"> 
+        <h3>About</h3>
+        <p>{apod.title}</p> 
+        <p>{apod.date}</p>
+        <p>{apod.explanation}</p>
+        <h6>copyright @ {apod.copyright}</h6>
       </p>
+    
+
     </div>
   );
 }
+//just need a pull request
 
 export default App;
